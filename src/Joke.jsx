df3eab3c831ceb91261434fc04d38eb13547e858
Sheldon01 <zhtm415@live.rhul.ac.uk>
2024-06-20 16:47:22 +0100

    added console log on line 4 in joke.jsx

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Joke(props) {
+    console.log("My props punchline was called and received " + props.punchline)
     return (
         <div className="joke">
             <h3>{props.setup}</h3>
@@ -8,5 +9,4 @@ function Joke(props) {
         </div>
     );
 }
-
 export default Joke;
